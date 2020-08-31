7f51277339ee7b14f344006426d6a3819e8099ff
andy8radshaw <andy8radshaw@gmail.com>
2020-08-31 08:56:40 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react'
 
+import Register from './components/auth/Register'
+
 const App = () => {
   return (
-    <h1>Hello World</h1>
+    <Register />
   )
 }
 
---
+++ src/components/auth/Register.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Register() {
+  return (
+    <h1>Hello there</h1>
+  )
+}
+
+export default Register
